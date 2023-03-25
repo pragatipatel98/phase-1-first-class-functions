e91fa7f0a1c34e977a1278d7ec5d27d615ab9e5b
Pragati Patel <pragatinpatel@gmail.com>
2023-03-25 09:35:31 -0400

    lab complete

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(cb) {
+    cb();
+}
+
+function returnsANamedFunction() {
+    return function named() {};
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {};
+}
